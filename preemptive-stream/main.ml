01b7af224da88decc03c9a2824289e1d8e1f9352
David Scott <dave@recoil.org>
2016-07-29 20:54:29 +0100

    Generalise the worker thread so we can run a function
    Signed-off-by: David Scott <dave@recoil.org>

--- preemptive-stream/main.ml
+++ preemptive-stream/main.ml
@@ -2,9 +2,12 @@
 open Lwt.Infix
 
 module Worker = (struct
-  type t = Thread.t * (unit Lwt.u option -> unit)
+  type ('a, 'b) t = {
+    processor: Thread.t;
+    push_request: ('a * 'b Lwt.u) option -> unit;
+  }
 
-  let make () =
+  let make f =
     let rec handle_requests requests =
       match Lwt_preemptive.run_in_main (fun () ->
         Lwt.catch
@@ -12,41 +15,42 @@ module Worker = (struct
           (fun _ -> Lwt.return None)
         ) with
       | None -> () (* exit thread *)
-      | Some result ->
+      | Some (request, result) ->
         (* Here's where we can do something in our background thread *)
+        let response = f request in
         Lwt_preemptive.run_in_main (fun () ->
-          Lwt.wakeup_later result ();
+          Lwt.wakeup_later result response;
           Lwt.return_unit
         );
         handle_requests requests in
     let requests, push_request = Lwt_stream.create () in
     let processor = Thread.create handle_requests requests in
-    processor, push_request
+    { processor; push_request }
 
-  let call (_, push_request) =
+  let call { push_request } x =
     let t, u = Lwt.task () in
-    push_request (Some u);
+    push_request (Some (x, u));
     t
 
-  let destroy (processor, push_request) =
+  let destroy { processor; push_request } =
     push_request None;
     Lwt_preemptive.detach Thread.join processor
 end: sig
-  type t
-  (** A background worker thread *)
+  type ('a, 'b) t
+  (** A background worker thread which runs a function from 'a -> 'b *)
 
-  val make: unit -> t
+  val make: ('a -> 'b) -> ('a, 'b) t
 
-  val call: t -> unit Lwt.t
+  val call: ('a, 'b) t -> 'a -> 'b Lwt.t
   (** Perform some work in the background worker thread *)
 
-  val destroy: t -> unit Lwt.t
+  val destroy: ('a, 'b) t -> unit Lwt.t
   (** Shutdown and join the background worker thread *)
 end)
 
 let one () =
-  let w = Worker.make () in
-  Worker.call w
+  let w = Worker.make (fun () -> ()) in
+  Worker.call w ()
   >>= fun () ->
   Worker.destroy w
 
